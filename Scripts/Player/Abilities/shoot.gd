0121764d6d45b88478276b965db0bd0e1451a99f
Qainguin <slimaster14@gmail.com>
2024-12-18 07:27:49 -0600

    Add Shooting

--- Scripts/Player/Abilities/shoot.gd
+++ Scripts/Player/Abilities/shoot.gd
@@ -2,6 +2,7 @@ extends RayCast3D
 
 const CLIP_SCALE = 0.05
 
+@onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var blaster_preloader: ResourcePreloader = $BlasterPreloader
 @onready var handle: Marker3D = $Handle
 
@@ -17,6 +18,10 @@ var blaster_node: Node3D = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_blaster = 0
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("shoot") and fire_rate_timer.is_stopped() and blaster_node:
+		print(shoot())
 
 func change_blaster(new_blaster: int) -> void:
 	var blaster: Node3D = blaster_preloader.load_blaster(new_blaster).instantiate() as Node3D
@@ -35,3 +40,18 @@ func change_blaster(new_blaster: int) -> void:
 	handle.add_child(blaster)
 	
 	blaster_node = blaster
+
+func shoot() -> Node3D:
+	fire_rate_timer.start()
+	
+	owner.rotation.y += randf_range(-0.015, 0.015)
+	owner.pivot.rotation.x += 0.015
+	
+	var rot_tween := get_tree().create_tween()
+	blaster_node.rotation.x = -0.1
+	rot_tween.tween_property(blaster_node, "rotation:x", 0.0, fire_rate_timer.wait_time)
+	
+	var collider: Node3D = null
+	if is_colliding():
+		collider = get_collider()
+	return collider
